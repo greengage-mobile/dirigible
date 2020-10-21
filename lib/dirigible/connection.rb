8cfaf311567129766fa943fa8c0d7825a60778dd
irwinchan <irwinchan@me.com>
2020-10-21 16:09:18 -0400

    Add Logging

--- lib/dirigible/connection.rb
+++ lib/dirigible/connection.rb
@@ -5,10 +5,15 @@ module Dirigible
     def connection
       options = { ssl: { min_version: OpenSSL::SSL::TLS1_2_VERSION, verify: true }, proxy: proxy }
       Faraday.new(endpoint, options) do |faraday|
+        logger.info(faraday.ssl)
         faraday.request(:json)
         faraday.basic_auth(app_key, master_secret)
         faraday.adapter(http_adapter)
       end
+    end
+
+    def logger
+      Logger.new('log/dirigible_ssl.log')
     end
   end
 end

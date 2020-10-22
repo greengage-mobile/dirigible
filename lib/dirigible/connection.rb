39d784564c2b399d4d17218eb25afbdbd5c27eb4
irwinchan <irwinchan@me.com>
2020-10-22 13:21:20 -0400

    Update Faraday SSLOptions to min version TLS 1.2
    * Update to Faraday SSLOptions to ensure TLS 1.2 is being used, required
    by Urban airship.
    
    * Add temporary logging to check if changes are being used.

--- lib/dirigible/connection.rb
+++ lib/dirigible/connection.rb
@@ -3,12 +3,18 @@ module Dirigible
   module Connection
     private
     def connection
-      options = { ssl: { verify: true }, proxy: proxy }
+      options = { ssl: { min_version: OpenSSL::SSL::TLS1_2_VERSION, verify: true }, proxy: proxy }
       Faraday.new(endpoint, options) do |faraday|
+        log_message = "Faraday SSLOptions: min_version >> #{faraday.ssl.min_version} <<"
+        logger.info log_message
         faraday.request(:json)
         faraday.basic_auth(app_key, master_secret)
         faraday.adapter(http_adapter)
       end
+    end
+
+    def logger
+      @logger ||= Sidekiq::Logging.logger
     end
   end
 end
